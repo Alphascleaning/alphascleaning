edbc1be8234d10b701e20cad5e9eddfffdbdafe3
Alphascleaning <ibrahimkamara161@gmail.com>
2026-07-06 12:53:07 +0200

    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,28 @@
+const CACHE_NAME = "alphas-cleaning-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/de/index.html",
+  "/en/index.html",
+  "/styles.css",
+  "/script.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
