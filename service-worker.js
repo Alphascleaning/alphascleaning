cae31880e473ce217c0b9bbd4c25db9c11bcf4f4
Alphascleaning <ibrahimkamara161@gmail.com>
2026-07-07 08:21:28 +0200

    Update cached files in service worker
    Removed unnecessary cached files and added new assets.

--- service-worker.js
+++ service-worker.js
@@ -3,11 +3,11 @@ const urlsToCache = [
   "/alphascleaning/",
   "/alphascleaning/index.html",
   "/alphascleaning/de/index.html",
-  "/alphascleaning/en/index.html",
-  "/alphascleaning/styles.css",
-  "/alphascleaning/script.js",
   "/alphascleaning/icons/icon-192.png",
-  "/alphascleaning/icons/icon-512.png"
+  "/alphascleaning/icons/icon-512.png",
+  "/alphascleaning/logo-header.png",
+  "/alphascleaning/favicon.png",
+  "/alphascleaning/about-alpha-photo.jpg"
 ];
 
 self.addEventListener("install", event => {
