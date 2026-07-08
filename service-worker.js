12e04b9eb5b6af134083d511fc7281cc8344cd88
Alphascleaning <ibrahimkamara161@gmail.com>
2026-07-08 13:43:42 +0200

    Refactor fetch event handling in service worker

--- service-worker.js
+++ service-worker.js
@@ -35,12 +35,12 @@ self.addEventListener("activate", event => {
     })
   );
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+      .then(response => response || caches.match("/offline.html"))
   );
 });
+
 
